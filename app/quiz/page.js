f482993639cbe8e2283af294f4078209a67ce3a0
Andrew-Reimert <andrew.reimert@edu.sait.ca>
2024-04-16 00:50:09 -0600

    Initial Creation

---
+++ app/quiz/page.js
@@ -0,0 +1,23 @@
+import {react} from 'react';
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center">
+    
+    <div className="max-w-xl">
+        <h1 className="text-4xl text-center">Quizzes</h1>
+      
+        <div className="flex items-center flex-col w-full p-4 mt-6 border-2">
+          <h2 className="text-2xl p-4">Filler</h2>
+          <p className="text-center">More Filler hehehehe</p>
+
+          <div className="flex justify-around">
+            
+          </div>
+        </div>
+
+      </div>
+    </main>
+  );
+
+}
