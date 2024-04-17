e57ea08ab5ed0ecd73b41e50a7dba28ecc1ac900
Andrew-Reimert <andrew.reimert@edu.sait.ca>
2024-04-17 14:23:34 -0600

    More Skeleton

--- app/page.js
+++ app/page.js
@@ -5,16 +5,16 @@ export default function Home() {
     <main className="flex flex-col items-center">
         <div className="w-xl">
       
-        <div className="flex items-center flex-col w-full p-4 mt-6 border-2 border-slate-500 bg-gray-950 rounded-lg">
+        <div className="flex items-center flex-col w-full p-12 mt-32 border-4 border-slate-500 bg-gray-950 rounded-lg">
           <h2 className="text-4xl p-6 text-center">Welcome to the Canadian Carrer Path Compass</h2>
-          <p className="text-center p-3">This system is designed to help you find the best option for your post-secondary education.</p>
-          <p className="text-center p-3">You can choose from the following:</p>
-          <p className="text-center p-3">Take one of our categorized quizzes to help you understand the field you want to go into, and which school fits you.</p>
-          <p className="text-center p-3">Browse available schools as well as use our filtering system to find what school fits your needs.</p>
+          <p className="text-lg text-center p-3">This system is designed to help you find the best option for your post-secondary education.</p>
+          <p className="text-lg text-center p-3">You can choose from the following:</p>
+          <p className="text-lg text-center p-3">Take one of our categorized quizzes to help you understand the field you want to go into, and which school fits you.</p>
+          <p className="text-lg text-center p-3">Browse available schools as well as use our filtering system to find what school fits your needs.</p>
 
-          <div className="flex justify-around p-6">
-          <div className="w-40 text-center m-3 p-3 border-2 bg-slate-900 hover:bg-slate-800 rounded-lg"><a href="/quiz">Take a Quiz!</a></div>
-          <div className="w-40 text-center m-3 p-3 border-2 bg-slate-900 hover:bg-slate-800 rounded-lg"><a href="/quiz">Browse Schools</a></div>
+          <div className="flex p-6 mt-4">
+          <div className="w-60 text-center m-5 p-6 border-2 border-slate-500 bg-slate-900 hover:bg-slate-800 rounded-lg"><a className="text-xl p-7 px-9" href="/quiz">Take a Quiz!</a></div>
+          <div className="w-60 text-center m-5 p-6 border-2 border-slate-500 bg-slate-900 hover:bg-slate-800 rounded-lg"><a className="text-xl" href="/quiz">Browse Schools</a></div>
           </div>
         </div>
 
@@ -22,4 +22,4 @@ export default function Home() {
     </main>
   );
 
-}
+} // The buttons aren't made correctly, something about the <a> tag placement but the div has to be first because flexbox so ill figure it out. I will fix the buttons in the next commit.
--- app/quiz/page.js
+++ app/quiz/page.js
@@ -3,21 +3,20 @@ import {react} from 'react';
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-    
-    <div className="max-w-xl">
-        <h1 className="text-4xl text-center">Quizzes</h1>
-      
-        <div className="flex items-center flex-col w-full p-4 mt-6 border-2">
-          <h2 className="text-2xl p-4">Filler</h2>
-          <p className="text-center">More Filler hehehehe</p>
+        <div className="w-auto+ flex items-center flex-col">
 
-          <div className="flex justify-around">
-            
+          <h2 className="text-4xl p-6 mt-7 text-center">Categorized Quizzes</h2>
+
+          <div className="flex items-center flex-col w-fit ">
+            <div className="w-full text-center m-5 p-6 border-2 border-slate-500 bg-gray-950 hover:bg-gray-800 rounded-lg"><a className="text-5xl" href='/'>Can you be a programmer?</a></div>
+            <div className="w-full text-center m-5 p-6 border-2 border-slate-500 bg-gray-950 hover:bg-gray-800 rounded-lg"><a className="text-5xl" href='/'>Can you be a doctor?</a></div>
+            <div className="w-full text-center m-5 p-6 border-2 border-slate-500 bg-gray-950 hover:bg-gray-800 rounded-lg"><a className="text-5xl" href='/'>Can you be a lawyer?</a></div>
           </div>
         </div>
-
-      </div>
     </main>
   );
 
 }
+
+// <div className="flex items-center flex-col w-full p-12 mt-32 border-4 border-slate-500 bg-gray-950 rounded-lg"> main content
+// <div className="w-60 text-center m-5 p-6 border-2 border-slate-500 bg-gray-950 hover:bg-gray-800 rounded-lg"> button
